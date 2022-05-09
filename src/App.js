8f422bf8173e4b7c3e4c0e1dbdb68b8e288831dd
Good Lookin <r_a_msmlm@outlook.com>
2022-05-09 14:35:22 +0300

    DONE TASK 1

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <title>Hiraeth</title>
-      <content>Your bakery</content>
+      <h1>H I R A E T H</h1>
+      <h4>Welcome Back Home</h4>
+      <img src="https://i.pinimg.com/736x/29/ce/df/29cedfa5902243d83f9d5b0f2cc1c8a2.jpg"/>
+
     </div>
   );
 }
